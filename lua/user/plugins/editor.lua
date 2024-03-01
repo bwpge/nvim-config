14152c7ed59dd821cf5d286415e8e0ce9dbf11ce
bwpge <bwpge.dev@gmail.com>
2024-02-29 20:24:17 -0500

    Add leap plugin

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -1,3 +1,5 @@
+local kmap = require("user.utils").kmap
+
 return {
     {
         "numToStr/Comment.nvim",
@@ -25,10 +27,27 @@ return {
         "windwp/nvim-autopairs",
         event = "VeryLazy",
         config = function()
-            require("nvim-autopairs").setup({})
+            require("nvim-autopairs").setup()
         end,
     },
     {
-        "antoinemadec/FixCursorHold.nvim",
+        "ggandor/leap.nvim",
+        event = "VeryLazy",
+        config = function()
+            -- basically the same as create_default_mappings,
+            -- just without `S` in `x` mode (nvim-surround)
+            local keys = {
+                { { "n", "x", "o" }, "s", "<Plug>(leap-forward)", "Leap forward" },
+                { { "n", "o" }, "S", "<Plug>(leap-backward)", "Leap backward" },
+                { { "n", "x", "o" }, "gs", "<Plug>(leap-from-window)", "Leap from window" },
+            }
+            for _, k in ipairs(keys) do
+                kmap(k[1], k[2], k[3], k[4])
+            end
+            require("leap").opts.special_keys.prev_target = "<bs>"
+            require("leap").opts.special_keys.prev_group = "<bs>"
+            require("leap.user").set_repeat_keys("<cr>", "<bs>")
+        end,
     },
+    { "antoinemadec/FixCursorHold.nvim" },
 }
