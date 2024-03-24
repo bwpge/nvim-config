d9839a03829128625e3587159cdb6c023862931f
bwpge <bwpge.dev@gmail.com>
2024-03-24 12:55:20 -0400

    Update formatter config and default treesitter parsers

--- lua/user/plugins/formatter.lua
+++ lua/user/plugins/formatter.lua
@@ -1,8 +1,11 @@
--- prettier adds trailing commas by default without config
-local function prettier_no_trailing_comma()
+-- formatter.nvim might configure prettier to use json5 parser, which adds
+-- trailing commas by default. prettier also won't format gitignored files by
+-- default so we need to use `--ignore-path=`.
+local function json_prettier()
     return {
         exe = "prettier",
         args = {
+            "--ignore-path=",
             "--stdin-filepath",
             vim.fn.shellescape(vim.api.nvim_buf_get_name(0), true),
         },
@@ -30,16 +33,17 @@ return {
                     require("formatter.filetypes.python").black,
                     require("formatter.filetypes.python").isort,
                 },
-                json = prettier_no_trailing_comma,
-                jsonc = prettier_no_trailing_comma,
+                json = json_prettier,
+                jsonc = json_prettier,
                 lua = { require("formatter.filetypes.lua").stylua },
                 toml = { require("formatter.filetypes.toml").taplo },
                 yaml = { require("formatter.filetypes.yaml").prettier },
+                -- allow tidy to clean up whitespace on FormatterPost instead of using sed
                 ["*"] = { function() end },
             },
         })
 
-        local formatter_group = vim.api.nvim_create_augroup("__formatter__", { clear = true })
+        local formatter_group = vim.api.nvim_create_augroup("NvimFormatter", { clear = true })
         local autocmd = vim.api.nvim_create_autocmd
 
         -- format on save
