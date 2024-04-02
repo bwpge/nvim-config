b629c777f9d54c60739200d0880690a864a90133
bwpge <bwpge.dev@gmail.com>
2024-04-01 22:33:31 -0400

    Add new lualine components

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -1,45 +1,38 @@
 local utils = require("user.utils")
 
-local function attached_lsp()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
-    if not clients or #clients == 0 then
-        return ""
-    end
-
-    local t = {}
-    for _, c in ipairs(clients) do
-        table.insert(t, c.name)
-    end
-    return "  " .. table.concat(t, ", ")
-end
+local empty_ext = {
+    sections = {},
+    filetypes = { "nerdtree", "neo-tree" },
+}
 
 return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = {
             "nvim-tree/nvim-web-devicons",
+            "bwpge/lualine-pretty-path",
         },
         lazy = false,
-        opts = utils.merge_custom_opts("lualine", {
-            options = {
-                component_separators = { left = "", right = "" },
-                section_separators = { left = "", right = "" },
-                disabled_filetypes = { "packer", "NVimTree", "neo-tree" },
-            },
-            sections = {
-                lualine_b = {
-                    { "branch", icon = "" },
-                    "diagnostics",
+        opts = function()
+            return utils.merge_custom_opts("lualine", {
+                options = {
+                    component_separators = { left = "", right = "" },
+                    section_separators = { left = "", right = "" },
                 },
-                lualine_c = {
-                    "filename",
+                sections = {
+                    lualine_b = {
+                        { "branch", icon = "" },
+                        "diagnostics",
+                    },
+                    lualine_c = {
+                        { "pretty_path", path_sep = "/" },
+                    },
+                    lualine_x = { "attached_lsp", "encoding" },
+                    lualine_y = { "harpoon", "fileformat", "progress" },
+                    lualine_z = { "location" },
                 },
-                lualine_x = { attached_lsp, "filetype" },
-                lualine_y = { "encoding", "fileformat", "progress" },
-                lualine_z = { "location" },
-            },
-            extensions = { "lazy" },
-        }),
+                extensions = { "lazy", empty_ext },
+            })
+        end,
     },
 }
