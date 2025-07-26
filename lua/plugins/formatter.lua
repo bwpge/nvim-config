2833f1a5e281e9addc964a61416785184cc7470d
bwpge <bwpge.dev@gmail.com>
2025-07-26 09:43:20 -0400

    Update formatter settings

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -4,11 +4,13 @@ return {
         event = "VeryLazy",
         opts = {
             formatters_by_ft = {
+                go = { "gofumpt", "golines" },
                 lua = { "stylua" },
+                python = { "black", "isort" },
                 ["_"] = { "trim_all" },
             },
             format_on_save = {
-                timeout_ms = 500,
+                timeout_ms = 5000,
                 lsp_format = "fallback",
             },
         },
